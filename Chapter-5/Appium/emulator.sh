ca815d94631536156de7bdb682a95b7afc8a02f0
evrass <77230105+evrass@users.noreply.github.com>
2025-04-08 18:57:17 +0000

    Update emulator.sh

--- Chapter-5/Appium/emulator.sh
+++ Chapter-5/Appium/emulator.sh
@@ -1,1 +1,11 @@
 
+#!/bin/bash 
+set -ex 
+npm install -g appium@${appium_version} --unsafe-perm=true --allow-root 
+if [ ${appium_version} = "next" ] 
+then 
+   appium driver install xcuitest 
+   appium driver install espresso 
+fi 
+
+appium --log appium.log &>/dev/null & 
